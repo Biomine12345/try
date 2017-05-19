22524c4fafec45dd83df12bcb85c3abb3ee31f23
Biomine12345 <biomine4@gmail.com>
2017-05-19 17:01:27 -0400

    h

--- biomine-view/inc/utils/xgettext_words.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * This file holds all words which we want to translate, but the xgettext utility fails to
- * extract due to various reasons (e.g. words in const arrays, words generated dynamically).
- */
-
-// Constants
-_('Biomine');
