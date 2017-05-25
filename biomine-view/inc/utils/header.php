f4f35a2f23e724e743a0405efa7f2347326ccf37
Biomine12345 <biomine4@gmail.com>
2017-05-25 18:03:28 -0400

    try

---
+++ biomine-view/inc/utils/header.php
@@ -0,0 +1,4 @@
+<?php
+namespace utils\header;
+
+include_once __DIR__ . '/../config.php';
---
+++ biomine-view/www/shop/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.biomine.us/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.biomine.us/');
+
+// DIR
+define('DIR_APPLICATION', __DIR__ . '/catalog/');
+define('DIR_SYSTEM', __DIR__ . '/system/');
+define('DIR_IMAGE', __DIR__ . '/image/');
+define('DIR_LANGUAGE', __DIR__ . '/catalog/language/');
+define('DIR_TEMPLATE', __DIR__ . '/catalog/view/theme/');
+define('DIR_CONFIG', __DIR__ . '/system/config/');
+define('DIR_CACHE', __DIR__ . '/system/storage/cache/');
+define('DIR_DOWNLOAD', __DIR__ . '/system/storage/download/');
+define('DIR_LOGS', __DIR__ . '/system/storage/logs/');
+define('DIR_MODIFICATION', __DIR__ . '/system/storage/modification/');
+define('DIR_UPLOAD', __DIR__ . '/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'de*m1u(2');
+define('DB_DATABASE', 'biomine');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'bio_');
